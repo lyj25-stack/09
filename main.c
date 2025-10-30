a2eaf60ce20421f12fee3ad1000433faf027ce8b
lyj25-stack <lyj25@sookmyung.ac.kr>
2025-10-30 15:49:06 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 
-	int testgrade[5];
+	int testscore[SIZE];
 	int i;
+	int sum=0;
+    printf("%i명의 점수를 입력하세요.\n");
 	
-	testgrade[0]=10;
-    testgrade[1]=20;
-	testgrade[2]=30;
-	testgrade[3]=40;
-	testgrade[4]=50;
-	for(i=0;i<5;i++)
-	printf("grade[%i]=%i\n",i,testgrade[i]);
+	for(i=0;i<SIZE;i++){
+			scanf("%d",&testscore[i]);
+			sum+=testscore[i];
+	}
+
+	#if 0
+	for(i=0;i<SIZE;i++)
+	printf("grade[%i]=%i\n",i,grade[i]);
+	#endif
+	
+	
+
 	
 	system("PAUSE");
 		return 0;
